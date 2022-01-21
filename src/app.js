216461ce485438765cfd8b9fd6ac52d98066ca61
ChristopherKRad <97244810+ChristopherKRad@users.noreply.github.com>
2022-01-20 21:34:12 -0800

    Added Box and Sphere to test
    Orbit Controls from @reactthreedrei not working properly

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
+import React, {Suspense} from "react";
 import "./styles.css";
 import styled from "styled-components";
 
 import Background from "./components/Background";
 import TextSection from "./components/TextSection";
+import Box from "./components/Box"
+import Sphere from "./components/AnimatedSphere";
+
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+
 
 export default function App() {
   return (
     <Wrapper className="App">
       <Background />
       <TextSection />
+      <Canvas classname="canvas">
+        <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1}/>
+        <Box/>
+      </Canvas>
+
+      <Canvas classname="canvas">
+        <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1}/>
+        <Sphere/>
+      </Canvas>
+
     </Wrapper>
   );
 }
@@ -16,4 +37,9 @@ export default function App() {
 const Wrapper = styled.div`
   position: relative;
   background: #1f1144;
+
+canvas {
+  height 500px;
+}
+
 `;
